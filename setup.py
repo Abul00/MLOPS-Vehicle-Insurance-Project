ffb384934a927a498712c343fcb65ca544197458
abul <abulhassan7411@gmail.com>
2025-11-19 07:01:24 +0530

    project structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Abul Hassan",
+    author_email="abulhassan7411@gmail.com",
+    packages=find_packages()
+)
